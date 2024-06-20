0d0c31062cbd71817cb621bee03f80e148a263a2
Djoni <crowndjoni@gmail.com>
2024-06-21 00:05:43 +0600

    task 93

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -14,7 +14,7 @@
     <form method="post" action="{{ route(app()->getLocale() . '.profile.update') }}" class="mt-6 space-y-6">
         @csrf
         @method('patch')
-
+            
         <div>
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full uk-input" :value="old('name', $user->name)"
@@ -51,9 +51,9 @@
         <div class="flex items-center gap-4">
             <x-primary-button class="uk-button uk-button-primary">{{ __('Save') }}</x-primary-button>
 
-            @if (session('status') === 'profile-updated')
-                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600">{{ __('Saved.') }}</p>
+            @if (session('status') == 'profile-updated')
+                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 4000)"
+                    class="text-sm text-gray-600">{{ __('Saved') }}</p>
             @endif
         </div>
     </form>
